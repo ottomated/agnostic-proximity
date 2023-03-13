7e2061c6bdf9a153b2e7dcb3a7ebb5d08b5c95d6
Ottomated <ottomated.57@gmail.com>
2023-03-12 17:29:44 -0700

    better lucide imports

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -6,6 +6,24 @@ const config = {
 	preprocess: [
 		preprocess({
 			postcss: true,
+			replace: [
+				[
+					/import\s*{\s*([^}]+?)\s*,?\s*}\s*from\s+['"]lucide-svelte['"](\s*;)?/gim,
+					(_, /**@type {string} */ imports) => {
+						return imports
+							.split(/\s*,\s*/gim)
+							.map((s) => {
+								let icon = s.split(/\s+as\s+/);
+								let name = icon[1] || icon[0];
+								let file = icon[0]
+									.replace(/[A-Z]/g, (s) => '-' + s.toLowerCase())
+									.slice(1);
+								return `import ${name} from 'lucide-svelte/dist/esm/icons/${file}.svelte';`;
+							})
+							.join('');
+					},
+				],
+			],
 		}),
 	],
 
