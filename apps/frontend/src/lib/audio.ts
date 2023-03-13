0bbaff272b9a7309463dfbc14ac1f48efbcd1363
Ottomated <ottomated.57@gmail.com>
2023-03-12 17:22:22 -0700

    immutable wav

--- apps/frontend/src/lib/audio.ts
+++ apps/frontend/src/lib/audio.ts
@@ -29,7 +29,7 @@ export function setStreamEnabled(
 function canAutoplay() {
 	const el = document.createElement('audio');
 	el.setAttribute('playsinline', 'playsinline');
-	el.src = '/a.wav';
+	el.src = '/_app/immutable/a.wav';
 	return new Promise<boolean>((resolve) => {
 		const playResult = el.play();
 		const timeout = setTimeout(() => {
